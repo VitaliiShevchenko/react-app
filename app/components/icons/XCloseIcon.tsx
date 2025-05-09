6927e9f8deb8e39d3acad90ade52cbe7764e2f8b
VitaliiShevchenko <iksel@ukr.net>
2025-05-09 16:32:58 +0300

    Add XCloseIcon component
    Introduce a reusable XCloseIcon component for a close button. It renders an SVG icon with white strokes, supporting customizable props for flexibility.

---
+++ app/components/icons/XCloseIcon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const XCloseIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        viewBox="0 0 24 24"
+        fill="none"
+        {...props}
+    >
+        <path d="M18 6L6 18M6 6L18 18" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+);
