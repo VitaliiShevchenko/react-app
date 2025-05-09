f5f6d6b75b812b91407ccbe7b463a89e6b5c2e9b
VitaliiShevchenko <iksel@ukr.net>
2025-05-09 09:10:40 +0300

    Change TableIcon

---
+++ app/components/icons/TableIcon.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export const TableIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={20}
+        height={20}
+        viewBox="0 0 20 20"
+        fill="none"
+        {...props}
+    >
+        <path d="M13.1051 17.0018C12.8043 17.0018 12.5012 16.9108 12.2408 16.7198C11.5919 16.247 11.4527 15.3435 11.9309 14.7019L12.7504 13.603C13.2286 12.9615 14.1424 12.8238 14.7912 13.2967C15.44 13.7695 15.5792 14.6731 15.101 15.3146L14.2816 16.4135C13.9964 16.7975 13.5542 16.9996 13.1051 16.9996V17.0018Z" fill="url(#paint0_linear_1_8134)"/>
+        <path d="M9.47928 17.0019C9.17843 17.0019 8.87534 16.9109 8.61491 16.72C7.96608 16.2472 7.82688 15.3436 8.30509 14.7021L12.8559 8.59943C13.3341 7.95787 14.2479 7.82023 14.8967 8.29308C15.5456 8.76593 15.6847 9.66945 15.2065 10.311L10.6557 16.4137C10.3706 16.7977 9.9283 16.9997 9.47928 16.9997V17.0019Z" fill="url(#paint1_linear_1_8134)"/>
+        <path d="M5.85849 16.9951C5.55764 16.9951 5.25455 16.9041 4.99412 16.7132C4.34529 16.2403 4.20609 15.3368 4.6843 14.6952L12.9642 3.5866C13.4425 2.94504 14.3562 2.80962 15.005 3.28025C15.6539 3.7531 15.7931 4.65662 15.3149 5.29819L7.03268 16.4068C6.74755 16.7909 6.30526 16.9929 5.85624 16.9929L5.85849 16.9951Z" fill="url(#paint2_linear_1_8134)"/>
+        <path d="M5.9614 11.9891C5.66056 11.9891 5.35747 11.8981 5.09703 11.7072C4.4482 11.2343 4.31125 10.3308 4.78721 9.68925L9.33804 3.5866C9.81625 2.94504 10.73 2.80962 11.3788 3.28025C12.0277 3.7531 12.1646 4.65662 11.6887 5.29819L7.13784 11.4008C6.85271 11.7849 6.41042 11.9869 5.9614 11.9869V11.9891Z" fill="url(#paint3_linear_1_8134)"/>
+        <path d="M6.06631 6.9854C5.76547 6.9854 5.46238 6.89438 5.20195 6.70347C4.55311 6.23284 4.41391 5.32932 4.89212 4.68775L5.71159 3.58888C6.18979 2.94509 7.10355 2.80967 7.75239 3.28252C8.40122 3.75537 8.53817 4.65889 8.06221 5.30046L7.24275 6.39933C6.95762 6.78338 6.51533 6.9854 6.06631 6.9854Z" fill="url(#paint4_linear_1_8134)"/>
+        <defs>
+            <linearGradient id="paint0_linear_1_8134" x1="13.516" y1="13.0155" x2="13.516" y2="17.0018" gradientUnits="userSpaceOnUse">
+                <stop stopColor="#00FF7B" />
+                <stop offset="1" stopColor="#00AEFF" />
+            </linearGradient>
+            <linearGradient id="paint1_linear_1_8134" x1="11.7558" y1="8.01187" x2="11.7558" y2="17.0019" gradientUnits="userSpaceOnUse">
+                <stop stopColor="#00FF7B" />
+                <stop offset="1" stopColor="#00AEFF" />
+            </linearGradient>
+            <linearGradient id="paint2_linear_1_8134" x1="9.99958" y1="3" x2="9.99958" y2="16.9951" gradientUnits="userSpaceOnUse">
+                <stop stopColor="#00FF7B" />
+                <stop offset="1" stopColor="#00AEFF" />
+            </linearGradient>
+            <linearGradient id="paint3_linear_1_8134" x1="8.23794" y1="3" x2="8.23794" y2="11.9891" gradientUnits="userSpaceOnUse">
+                <stop stopColor="#00FF7B" />
+                <stop offset="1" stopColor="#00AEFF" />
+            </linearGradient>
+            <linearGradient id="paint4_linear_1_8134" x1="6.47668" y1="3.00067" x2="6.47668" y2="6.9854" gradientUnits="userSpaceOnUse">
+                <stop stopColor="#00FF7B" />
+                <stop offset="1" stopColor="#00AEFF" />
+            </linearGradient>
+        </defs>
+    </svg>
+);
