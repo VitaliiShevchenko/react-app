e32475f75ff216d15c70935d16d448bc466b5d2a
VitaliiShevchenko <iksel@ukr.net>
2025-05-09 15:14:44 +0300

    Create the DropDowmArrow Icon

---
+++ app/components/icons/DropDowmArrow.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const DropDowmArrow = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={20}
+        height={20}
+        viewBox="0 0 20 20"
+        fill="none"
+        {...props}
+    >
+        <path d="M15.8333 7.50001L9.99998 13.3333L4.16665 7.50001" stroke="#475467" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+);
