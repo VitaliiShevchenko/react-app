87d9357dafb4c63462635e7c3b19ce193f31778b
VitaliiShevchenko <iksel@ukr.net>
2025-05-09 16:30:44 +0300

    Add PlusIcon component
    Introduce a reusable PlusIcon component using an SVG element. This can be used across the app where a plus icon is required, ensuring consistency and reusability.

---
+++ app/components/icons/PlusIcon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const PlusIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={16}
+        height={16}
+        viewBox="0 0 16 16"
+        fill="none"
+        {...props}
+    >
+        <path d="M7.99999 2.66663V13.3333M2.66666 7.99996H13.3333" stroke="#1A292B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+);
