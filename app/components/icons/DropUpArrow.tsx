23036d256466a71f7852f3284be4355b962d3b65
VitaliiShevchenko <iksel@ukr.net>
2025-05-09 15:28:36 +0300

    Create the DropUpArrow component

---
+++ app/components/icons/DropUpArrow.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const DropUpArrow = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={24}
+        height={24}
+        viewBox="0 0 24 24"
+        fill="none"
+        {...props}
+    >
+        <path d="M5 15L12 8L19 15" stroke="#677281" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+);
