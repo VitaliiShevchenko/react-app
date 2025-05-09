c302bb850eda64c577b41304c483f77f416c7b11
VitaliiShevchenko <iksel@ukr.net>
2025-05-09 15:18:22 +0300

    Create the queryBuilder - the file of types

--- app/types/queryBuilder.tsx
+++ app/types/queryBuilder.tsx
@@ -4,11 +4,37 @@ export interface Field {
   type: string;
   table: string;
 }
+export interface Column {
+  name: string;
+  type: 'string' | 'number' | 'date' | 'boolean';
+  required?: boolean;
+}
+
+export interface TableDefinition {
+  columns: Column[];
+}
+
+export type DataRecord = {
+  [key: string]: string | number | boolean | Date;
+}
+
+type InferTableData<T extends TableDefinition> = {
+  [K in T['columns'][number]['name']]:
+  T['columns'][number] extends { name: K, type: 'string' } ? string :
+      T['columns'][number] extends { name: K, type: 'number' } ? number :
+          T['columns'][number] extends { name: K, type: 'boolean' } ? boolean :
+              T['columns'][number] extends { name: K, type: 'date' } ? Date :
+                  never;
+};
+
 
 export interface Table {
+  id: string;
   name: string;
-  fields: Field[];
+  definition: TableDefinition;
+  data: DataRecord[] | InferTableData<TableDefinition>[];
 }
+
 
 export type JoinType = 'INNER' | 'LEFT' | 'RIGHT' | 'FULL';
 
